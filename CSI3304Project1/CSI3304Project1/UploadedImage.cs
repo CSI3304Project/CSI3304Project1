1e1394650f81ac93fc589a14e1aaa51d1bf22065
aaronmoore93 <aaron.moore.it@gmail.com>
2014-09-23 10:14:03 +0800

    Added classes for Images and Users

---
+++ CSI3304Project1/CSI3304Project1/UploadedImage.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace CSI3304Project1
+{
+    public class UploadedImage
+    {
+        private int imageID;
+        private DateTime imageUploadDate;
+        private string imageProvider;
+        private ArrayList imageTags;
+        private string imageStatus;
+        private Image imageFile;
+
+        public UploadedImage()
+        {
+            imageID = 0;
+            imageUploadDate = new DateTime(2001, 01, 01);
+            imageProvider = "Username01";
+            imageTags = new ArrayList();
+            imageTags.Add("tag");
+            imageStatus = "Unmoderated";
+        }
+
+        public void setImageID(int inputID)
+        {
+            imageID = inputID;
+        }
+
+        public void setImageUploadDate(DateTime inputDate)
+        {
+            imageUploadDate = inputDate;
+        }
+
+        public void setImageProvider(string inputProvider)
+        {
+            imageProvider = inputProvider;
+        }
+
+        public void setImageTags(ArrayList inputTags)
+        {
+            imageTags = inputTags;
+        }
+
+        public void setImageStatus(string inputStatus)
+        {
+            imageStatus = inputStatus;
+        }
+
+        public int getImageID()
+        {
+            return imageID;
+        }
+
+        public DateTime getImageUploadDate()
+        {
+            return imageUploadDate;
+        }
+
+        public string getImageProvider()
+        {
+            return imageProvider;
+        }
+
+        public ArrayList getImageTags()
+        {
+            return imageTags;
+        }
+
+        public string getImageStatus()
+        {
+            return imageStatus;
+        }
+    }
+}
---
+++ CSI3304Project1/CSI3304Project1/User.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSI3304Project1
+{
+    public class User
+    {
+        private string userUsername;
+        private string userFirstName;
+        private string userLastName;
+        private string userEmail;
+        private string userType;
+
+        public User()
+        {
+            userUsername = "Username01";
+            userFirstName = "First";
+            userLastName = "Last";
+            userEmail = "email@email.com";
+            userType = "user";
+        }
+
+        public void setUsername(string inputUsername)
+        {
+            userUsername = inputUsername;
+        }
+
+        public void setFirstName(string inputFirstName)
+        {
+            userFirstName = inputFirstName;
+        }
+
+        public void setLastName(string inputLastName)
+        {
+            userLastName = inputLastName;
+        }
+
+        public void setEmail(string inputEmail)
+        {
+            userEmail = inputEmail;
+        }
+
+        public void setType(string inputType)
+        {
+            userType = inputType;
+        }
+
+        public string getUsername()
+        {
+            return userUsername;
+        }
+
+        public string getFirstName()
+        {
+            return userFirstName;
+        }
+
+        public string getLastName()
+        {
+            return userLastName;
+        }
+
+        public string getEmail()
+        {
+            return userEmail;
+        }
+
+        public string getType()
+        {
+            return userType;
+        }
+    }
+}
